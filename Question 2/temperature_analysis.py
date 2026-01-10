899931c6bd8eea42a3307d4b03c0ebea60c647ce
komal poudel <poudelkamal110@gmail.com>
2026-01-10 23:15:28 +0930

    Load all CSV files from temperatures folder

--- Question 2/temperature_analysis.py
+++ Question 2/temperature_analysis.py
@@ -1,9 +1,22 @@
 import os
 import pandas as pd
 import numpy as np
- 
+
+def load_temperatures_data(folder_path):
+    all_data = []
+
+    for file in os.listdir(folder_path):
+        if file.endswith(".csv"):
+            file_path = os.path.join(folder_path, file)
+            df = pd.read_csv(file_path)
+            all_data.append(df)
+
+    combined_df = pd.concat(all_data, ignore_index=True)
+    return combined_df
+
 def main():
-    print("Temperature analysis started")
+    df = load_temperatures_data("temperatures")
+    print(df.head())
 
 if __name__ == "__main__":
     main()
